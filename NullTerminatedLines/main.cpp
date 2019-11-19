385f7e097d3c9c8c8a340c571b2435ed9f1f37f0
Student <Student@304-1>
2019-11-19 20:53:53 +0200

    second

--- NullTerminatedLines/main.cpp
+++ NullTerminatedLines/main.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
+#include<Windows.h>
 using namespace std;
 
+void input_line(char str[], const int n);
+int StrLen(char[]);
 
 void main()
 {
@@ -13,7 +16,20 @@ void main()
 	char str[n];
 	cout << "¬ведите строку: ";
 	//cin >> str;
-	cin.getline(str, n);
+	input_line(str, n);
 	cout << str << endl;
+	cout << StrLen(str) << endl; //проверка размера
 
+}
+void input_line(char str[], const int n)
+{
+	SetConsoleCP(1251); //кодтровка
+	cin.getline(str, n);
+	SetConsoleCP(866); //–усс дл€ консоли
+}
+int StrLen(char str[])
+{
+	int i = 0;
+	for (; str[i]; i++); //будет выполн€тс€ пока не ноль
+	return i;
 }
