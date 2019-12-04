69907deb10471f3079b62b109a7ceff40f64a92e
Student <Student@304-1>
2019-12-04 20:49:14 +0200

    homework

--- NumericSystems/main.cpp
+++ NumericSystems/main.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 #include<Windows.h>
-using namespace std;
+//using namespace std;
 
 bool isNomber(char str[]);
 int StrToInt(char str[]);
 char* dec_to_bin(int dec);
-bool isHex(char str[]);
+bool isHex(const char* str);
 int hex_to_dec(char str[]);
+
+bool is_hex_number(const char* str);
+int hex_to_dec2(const char* str);
 
 #define TAB "\t"
 
@@ -21,10 +24,18 @@ void main()
 	const int n = 100;
 	char str[n];
 
-	cout << "¬ведите 16ричную строку: ";
-	cin >> str;
-	int hex = hex_to_dec(str);
-	cout << hex << endl;
+	//std::cout << "¬ведите 16ричную строку: ";
+	//std::cin >> str;
+	//int hex = hex_to_dec(str);
+	//std::cout << hex << std::endl;
+
+	const int q = 9;
+	char her[q];
+	std::cout << "¬ведите 16ричную строку: "; std::cin >> her;
+	std::cout << is_hex_number(her) << std::endl;
+	int hex = hex_to_dec(her);
+	std::cout << hex << std::endl;
+	
 }
 char* dec_to_bin(int dec) 
 {
@@ -39,12 +50,14 @@ char* dec_to_bin(int dec)
 	}
 	cout << endl;*/
 	n = strlen(binary);
-	for (int i = 0; i < n / 2; i++) swap(binary[i], binary[n - 1 - i]);
+	for (int i = 0; i < n / 2; i++) std::swap(binary[i], binary[n - 1 - i]);
 	return binary;
 }
-bool isHex(char str[])
+bool isHex(const char* str)
 {
-	for (int i = 0; str[i]; i++)
+	int i = 0;
+	if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X')) i = 2;
+	for (; str[i]; i++)
 	{
 		if (!(str[i] >= '0' && str[i] <= '9' ||
 			str[i] >= 'A' && str[i] <= 'F' ||
@@ -52,26 +65,71 @@ bool isHex(char str[])
 	}
 	return true;
 }
+//int hex_to_dec(char str[])
+//{
+//	int num = 0;
+//	int pow = 1;
+//	int otvet = 0;
+//	if (isHex(str))
+//	{
+//		for (int i = strlen(str) - 1; i >= 0; i--, pow *= 16)
+//		{
+//			if (str[i] >= '0' && str[i] <= '9') num = str[i] - 48;
+//			else if (str[i] >= 'A' && str[i] <= 'F') num = str[i] - 55;
+//			else num = str[i] - 87;
+//			int buffer = num;
+//			if (pow != 1) for (int j = 0; j < pow - 1; j++)
+//			{
+//				num += buffer;
+//			}
+//			otvet += num;
+//		}
+//	}
+//	return otvet;
+//}
 int hex_to_dec(char str[])
 {
 	int num = 0;
 	int pow = 1;
-	int otvet = 0;
 	if (isHex(str))
 	{
 		for (int i = strlen(str) - 1; i >= 0; i--, pow *= 16)
 		{
-			if (str[i] >= '0' && str[i] <= '9') num = str[i] - 48;
-			else if (str[i] >= 'A' && str[i] <= 'F') num = str[i] - 55;
-			else num = str[i] - 87;
-			int buffer = num;
-			if (pow != 1) for (int j = 0; j < pow - 1; j++)
-			{
-				num += buffer;
-			}
-			otvet += num;
+			if (str[i] >= '0' && str[i] <= '9') str[i] = str[i] - 48;
+			else if (str[i] >= 'A' && str[i] <= 'F') str[i] = str[i] - 55;
+			else str[i] = str[i] - 87;
+			int buffer = str[i];
+			if (pow != 1) for (int j = 0; j < pow - 1; j++)	str[i] += buffer;
+			num += str[i];
 		}
 	}
-	return otvet;
+	return num;
 }
 ////////////////////////////////
+bool is_hex_number(const char* str)
+{
+	int i = 0;
+	if (str[0] == '0' && (str[1] == 'x'|| str[1] == 'X')) i = 2;
+	for (; str[i]; i++)
+	{
+		if (!(str[i] >= '0' && str[i] <= '9') &&
+			!(str[i] >= 'A' && str[i] <= 'F') &&
+			!(str[i] >= 'a' && str[i] <= 'f')) return false;
+	}
+	return true;
+}
+int hex_to_dec2(const char* her)
+{
+	if (!is_hex_number(her)) return 0;
+	int decimal = 0;
+	int pow = 1;
+	for (int i = her[1] == 'x' || her[1] == 'X' ? 2 : 0;her[i]; i++)
+	{
+		pow *= 16;
+		if (her[i] >= '0'&&her[i] >= '9') decimal += her[i] - '0';
+		else if (her[i] >= 'A'&&her[i] >= 'F') decimal += her[i] - 'A' + 10;
+		else if (her[i] >= 'a'&&her[i] >= 'f') decimal += her[i] - 'a' + 10;
+	}
+	
+	return decimal;
+}
---
+++ Project1StandartStrFunctioms/main.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 256;
+	/*
+	char str[n]{};
+	std::cout << "Введите строчку: "; std::cin.getline(str, n);
+	std::cout << str << std::endl;
+	std::cout << strlen(str) << std::endl;
+
+	char buffer[n] = {};
+	strcpy(buffer, str);
+	std::cout << buffer << std::endl;*/
+
+	//char str1[n] = "Hello";
+	//char str2[] = "World";
+	//strcmp(str1, str2);
+	//std::cout << str1 << std::endl;
+
+	const char* words[] =
+	{
+		"Hello",
+		"World",
+		"computer",
+		"C++",
+		"December"
+	};
+	for (int i = 0; i < sizeof(words) / sizeof(const char*); i++)
+	{
+		std::cout << words[i] << std::endl;
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < sizeof(words) / sizeof(const char*); i++)
+	{
+		for (int j = i + 1; j < sizeof(words) / sizeof(const char*); j++)
+		{
+			// ето не строка
+			//запомнит прошлую строку
+			if (words[j] == '\0')
+			{
+				if (words[i] < words[j])
+				{
+					const char* buffer = words[i];
+					words[i] = words[j];
+					words[j] = buffer;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < sizeof(words) / sizeof(const char*); i++)
+	{
+		std::cout << words[i] << std::endl;
+	}
+}
