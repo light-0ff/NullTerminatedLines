71950a09514831ea37cfe68ccf7d8c0315fa08c5
Student <Student@304-1>
2019-12-11 21:07:10 +0200

    123

--- Files/main.cpp
+++ Files/main.cpp
@@ -7,8 +7,9 @@
 
 //#define write_to_file
 //#define read_from_file
-#define switchs
+//#define switchs
 //#define create
+//#define domashka
 
 void main()
 {
@@ -73,7 +74,7 @@ void main()
 	fin.close();
 
 	fout.close(); 
-	char notepad[n] = "notepad ";
+	char notepad[] = "notepad ";
 	strcat(notepad, filename);
 	system(notepad);
 	//system("notepad list.txt");
@@ -90,7 +91,87 @@ void main()
 	fout << "Sam privet" << std::endl;
 	fout.close();
 #endif // create
+#ifdef domashka
+	std::ifstream fin;
 
+	const int n = 256;
+	char source_name[n] = {};
+	std::cout << "Напиши: result  "; std::cin.getline(source_name, n);
+	if (!strstr(source_name, ".txt"))strcat(source_name, ".txt"); // для работы с тхт файлами
+	fin.open(source_name);
 
+	char buffer_1[40] = {};
+	char buffer_2[40] = {};
+
+	char filename[n]{}; //новый документ для переноса
+	std::cout << "Напиши: list  "; std::cin.getline(filename, n);
+	std::ofstream fout;
+	if (!strstr(filename, ".txt"))strcat(filename, ".txt"); //проверка на разширение .txt
+	fout.open(filename);// сюда записываем
+
+	//for (int i = 0; source_name[i]; i++)
+	//{
+	//	fin >> buffer_1;	
+	//	fin >> buffer_2;
+	//	fout << buffer_1 << " ";
+	//	fout << buffer_2 << std::endl;
+	//	//if(buffer_1 == "{") // ненаходит {
+	//	//{
+	//	//	fout << std::endl << buffer_1 << std::endl;
+	//	//}
+	//	//else 	fout << buffer_1 << " ";
+	//}
+	
+	while (!fin.eof())
+	{
+		fin >> buffer_1; // копируем до пробела
+		fin >> buffer_2; //копируем после пробела
+		if (buffer_1 != "{" && buffer_1 != "}")	fout << buffer_1 << " ";
+		else  fout << std::endl << buffer_1 << std::endl;
+		if (buffer_2 != "{" && buffer_2 != "}")	fout << buffer_2 << std::endl;
+		else  fout << std::endl << buffer_2 << std::endl;
+	}
+	fin.close();
+
+	fout.close();
+	char notepad[n] = "notepad ";
+	strcat(notepad, filename);
+	system(notepad);
+#endif // domashka
+	const int n = 256;
+	char source_file[n]{};
+	char dest_file[n]{};
+	std::cout << "Введите имя соуса: "; std::cin >> source_file;
+	std::cout << "Введите имя конечного файла: "; std::cin >> dest_file;
+	if (!strstr(source_file, ".txt"))strcat(source_file, ".txt"); 
+	if (!strstr(dest_file, ".txt"))strcat(dest_file, ".txt"); 
+
+	std::ifstream fin(source_file);
+	std::ofstream fout(dest_file);
+
+	char mac_buffer[n]{};
+	char ip_buffer[n]{};
+		
+	for (int i=1; !fin.eof();i++)
+	{
+		fin >> mac_buffer;
+		fin >> ip_buffer;
+		fout << "host-" << i << std::endl;
+		fout<<"{\n";
+		fout << "\thehardware-ethernet\t" << mac_buffer << ";\n";
+		fout << "\tfixed-address\t" << ip_buffer << ";\n";
+		fout << "}\n";
+
+		//fin >> ip_buffer;
+		std::cout << "host-" << i << std::endl;
+		std::cout << "{\n";
+		std::cout << "\thehardware-ethernet\t" << mac_buffer << ";\n";
+		std::cout << "\tfixed-address\t" << ip_buffer << ";\n";
+		std::cout << "}\n";
+	}
+	fin.close();
+	fout.close();
+	char command[n] = "system ";
+	strcat(command, dest_file);
 
 }
